151643e8e742d1c28b7f1c4c78c79a0f830f77f5
fatema <sadeghifatema@yahoo.com>
2012-05-06 10:36:01 +0430

    'validation!'

--- test/unit/cm_test.rb
+++ test/unit/cm_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class CmTest < ActiveSupport::TestCase
+	fixtures :cms
   # test "the truth" do
   #   assert true
-  # end
+  #end
+
+  test "cm attributes must not be empty" do
+cm = Cm.new
+assert cm.invalid?
+assert cm.errors[:title].any?
+assert cm.errors[:permalink].any?
+assert cm.errors[:body].any?
+assert cm.errors[:created_at].any?
+assert cm.errors[:updated_at].any?
+end
+test "cm is not valid without a unique title" do
+cm = cm.new(:title => cm(:ruby).title,
+:description => "yyy",)
+assert !cm.save
+assert_equal "has already been taken", cm.errors[:title].join('; ')
+end
 end
